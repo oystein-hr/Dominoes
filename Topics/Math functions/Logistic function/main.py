e0482054b0568fae3842969a9621ec0b88920e90
oystein-hr <oystein.github@hofvind-rom.com>
2021-11-04 10:55:47 +0100

    Remove ignored files

--- Topics/Math functions/Logistic function/main.py
+++
@@ -1,5 +0,0 @@
-import math
-
-x = int(input())
-result = 1 / (1 + math.exp(-x))
-print(round(result, 2))
--- Topics/Math functions/The area of a circle/main.py
+++
@@ -1,6 +0,0 @@
-import math
-
-r = int(input())
-area = math.pi * math.pow(r, 2)
-
-print(round(area, 2))
